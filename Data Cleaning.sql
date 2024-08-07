ce6a5b6d4e5ad4b7576023d0a1bb31bd13a710f4
Winnie CIRINO <136930448+WinnieCIRINO@users.noreply.github.com>
2024-08-07 04:37:00 +0200

    Update Data Cleaning.sql

--- Data Cleaning.sql
+++ Data Cleaning.sql
@@ -1,3 +1,8 @@
+Data Cleaning Objective: 
+To identify data quality issues such as null cells, unstandardized formatting, unusual characteries and inconsistencies that could affect the data quality and integrity. 
+Subsequently, to develop ways to mitigate them, and ensure that the data is suitable and reliable for analysis. The tool used for this integral part of data analysis is SQL.
+
+
 USE MiniProject4_AdventureWorks
 
 -- Joining the SalesData tables
